e2e190ff40278e71f6b826f1e5729eb528005afa
Grigore Stefan <g_stefan@yahoo.com>
2021-07-05 16:51:32 +0300

    update

---
+++ build/msvc.install.cmd
@@ -0,0 +1,14 @@
+@echo off
+rem Public domain
+rem http://unlicense.org/
+rem Created by Grigore Stefan <g_stefan@yahoo.com>
+
+echo -^> install vendor-openssh
+
+set INSTALL_PATH=%XYO_PATH_REPOSITORY%
+set INSTALL_PATH_BIN=%XYO_PATH_REPOSITORY%\opt\openssh
+set INSTALL_PATH_DEV=%INSTALL_PATH_BIN%
+
+rem // ---
+
+call build\msvc.install.sub.cmd
---
+++ build/msvc.install.sub.cmd
@@ -0,0 +1,14 @@
+@echo off
+rem Public domain
+rem http://unlicense.org/
+rem Created by Grigore Stefan <g_stefan@yahoo.com>
+
+rem --- make
+
+cmd.exe /C build\msvc.make.cmd
+if errorlevel 1 exit 1
+
+rem ---
+
+if not exist "%INSTALL_PATH_BIN%\" mkdir "%INSTALL_PATH_BIN%"
+xcopy /Y /S /E "temp\openssh\*.*" "%INSTALL_PATH_BIN%\"
